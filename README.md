7d3a996f62e57835a506f3cc13b90672e0490c3c
Nakorock <kobayashi.hayate@logosware.com>
2025-02-25 12:57:43 +0900

    拡張機能の実装

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # at-saver README
 
-This is the README for your extension "at-saver". After writing up a brief description, we recommend including the following sections.
+
 
 ## Features
 
--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,54 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
+    let timeoutId: NodeJS.Timeout;
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "at-saver" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('at-saver.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from at-saver!');
-	});
+    const stopDisposable = vscode.commands.registerCommand('at-saver.stop', () => {
+        // 拡張機能の動作を停止
+        clearTimeout(timeoutId);
+        vscode.window.showInformationMessage('at-saver stopped.');
+    });
+    const disposable = vscode.commands.registerCommand('at-saver', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+
+            let isDeleted = false;
+
+            const randomInterval = () => Math.floor(Math.random() * (7 - 3 + 1) + 3) * 60 * 10;
+            const position = editor.selection.active;
+            const line = document.lineAt(position.line);
+            const lineText = line.text;
+            const executeTask = async () => {
+                timeoutId = setTimeout(executeTask, randomInterval());
+                if (isDeleted) {
+                    editor.edit(editBuilder => {
+                        editBuilder.insert(position, lineText);
+                    });
+                } else {
+                    const range = new vscode.Range(line.range.start, line.range.end);
+                    editor.edit(editBuilder => {
+                        editBuilder.delete(range);
+                    });
+                }
+                isDeleted = !isDeleted;
+                // 1秒まつ
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                document.save();
+
+            };
+            executeTask();
+        }
+    });
 
 	context.subscriptions.push(disposable);
+    context.subscriptions.push(stopDisposable);
 }
 
 // This method is called when your extension is deactivated
